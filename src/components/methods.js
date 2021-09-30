dfb6e40949177ad46fac144274e57da25652e13f
Richard Leys <leys.richard@gmail.com>
2021-09-30 19:10:52 +0200

    Updated to Plotly 2.3.0

--- src/components/methods.js
+++ src/components/methods.js
@@ -3,7 +3,7 @@ import Plotly from "plotly.js-basic-dist-min";
 const plotlyFunctions = ["restyle", "relayout", "update", "addTraces", "deleteTraces", "moveTraces", "extendTraces", "prependTraces", "purge"];
 
 const methods = plotlyFunctions.reduce((all, functionName) => {
-  all[functionName] = function(...args) {
+  all[functionName] = function (...args) {
     return Plotly[functionName].apply(Plotly, [this.$el, ...args]);
   };
   return all;
